d4de316c657aed984c45d6d00d43ae2c92a20e39
cauaMascena <cauasinho656@gmail.com>
2021-09-20 11:15:26 -0300

    envio 20/09/2021

---
+++ app/src/main/java/com/example/projetoimc/DashView_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.projetoimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashView_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash_view_)
+    }
+}
