6f20c7a0e4f1e90f1a193a1a29df6c60da423760
cauaMascena <cauasinho656@gmail.com>
2021-10-18 11:15:44 -0300

    commit 18/10/2021

--- app/src/main/java/com/example/projetoimc/LoginActivity.kt
+++ app/src/main/java/com/example/projetoimc/LoginActivity.kt
@@ -1,18 +1,24 @@
 package com.example.projetoimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
-class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class LoginActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val tvNovaConta = findViewById<TextView>(R.id.tv_nova_conta)
+        val tvHello = findViewById<TextView>(R.id.tv_criar_conta)
+        tvHello.setOnClickListener {
+            val abrirActivityCadastro = Intent(this, PerfilActivity::class.java)
+            //contexto e "destino" ||||| ::class.java - instancia a classe
 
-        tvNovaConta.setOnClickListener {
-        val novaConta = intent(this, "NovoUsuario")
+            startActivity(abrirActivityCadastro)
         }
     }
+
 }
--- app/src/main/java/com/example/projetoimc/PerfilActivity.kt
+++ app/src/main/java/com/example/projetoimc/PerfilActivity.kt
@@ -1,11 +1,70 @@
 package com.example.projetoimc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_perfil.*
+import java.util.*
 
 class PerfilActivity : AppCompatActivity() {
+
+    lateinit var editEmail: EditText
+    lateinit var editSenha: EditText
+    lateinit var editNome: EditText
+    lateinit var editProfi: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
+
+        editEmail = findViewById(R.id.edit_email)
+        editSenha = findViewById(R.id.edit_senha)
+        editNome = findViewById(R.id.edit_nome)
+        editProfi = findViewById(R.id.edit_profissao)
+
+    }
+    //criando calendário
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu_salvar, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (editEmail.text.isEmpty()){
+            editEmail.error = "O e-mail é obrigatório!"
+        }
+        if (editSenha.text.isEmpty()){
+            editSenha.error = "A senha é obrigatória!"
+        }
+        if (editNome.text.isEmpty()){
+            editNome.error = "O nome é obrigatório!"
+        }
+        if (editProfi.text.isEmpty()){
+            editProfi.error = "A profissão é obrigatória!"
+        }
+
+        return true
+    }
+    fun ValidarCampos() : Boolean {
+        var valido = true
+        if (editEmail.text.isEmpty()) {
+            editEmail.error = "E-mail é obrigatório"
+        }
+        if (editSenha.text.isEmpty()) {
+            editSenha.error = "Senha é obrigatório"
+        }
+        if (editNome.text.isEmpty()) {
+            editNome.error = "Nome é obrigatório"
+        }
+        return valido
     }
 }
